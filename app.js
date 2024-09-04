6111bf914291f25fbff7c7c6a2d95bf1d331c948
chinmay-2402 <chavanchinmay2424@gmail.com>
2024-09-04 11:46:16 +0000

    commit from server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello everyone');
+  res.send('Hello all');
 });
 
 // Define the port number and start the server
